b9e03cfb98c905a71b04f68d088fd1001a480c2a
Alessandro <alessandro.ceccato.3@gmail.com>
2022-09-29 02:32:14 +0200

    Update external library

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,23 @@
 
 
 using cpputils::_;  // NOLINT
+using cpputils::i32;
 using namespace cpputils::literals;
 
+struct Integer {
+public:
+    constexpr explicit Integer(i32 value)
+        : m_value{value} {}
+    [[nodiscard]] constexpr i32 get() const noexcept { return m_value; }
+
+private:
+    i32 m_value{};
+};
+
 int main() {
-    static constexpr auto v = std::array{1_i32, -9_i32, 6_i32, 5_i32};
-    static constexpr auto negative = std::ranges::find_if(v, _ < 0_i32);
+    static constexpr auto v = std::array{Integer{1_i32}, Integer{-9_i32}, Integer{6_i32}, Integer{5_i32}};
+    static constexpr auto negative = std::ranges::find_if(v, _.fn(&Integer::get) < 0_i32);
     if (negative != v.end()) {
-        std::cout << "Found a negative number: " << *negative << '\n';
+        std::cout << "Found a negative number: " << negative->get() << '\n';
     }
 }
