305a846461fb955ab44832d67694163c4d7e7d36
Alessandro <alessandro.ceccato.3@gmail.com>
2022-10-02 11:12:46 +0200

    Add benchmark timer example

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "cpputils/functional/operator_sections.hpp"
+#include "cpputils/misc/benchmark.hpp"
 #include "cpputils/types/number.hpp"
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 
 using cpputils::_;  // NOLINT
 using cpputils::i32;
 using namespace cpputils::literals;
+using namespace cpputils::benchmark;
+
+using namespace std::literals;
 
 struct Integer {
 public:
@@ -25,4 +31,27 @@ int main() {
     if (negative != v.end()) {
         std::cout << "Found a negative number: " << negative->get() << '\n';
     }
+
+    std::cout << "\n\n-------------\nBenchmark test\n-------------\n\n";
+
+    auto logger = timer<>::logger_t{};
+    {
+        timer<>::scoped const t{logger, "section_1"};
+        std::this_thread::sleep_for(350ms);
+    }
+    {
+        timer<>::scoped const t{logger, "section_2"};
+        std::this_thread::sleep_for(100ms);
+    }
+    {
+        timer<>::scoped const t{logger, "section_3"};
+        std::this_thread::sleep_for(503ms);
+    }
+    {
+        timer<>::scoped const t{logger, "section_4"};
+        std::this_thread::sleep_for(236005000ns);
+    }
+    auto const time_report = reporter<>::report<formatters::markdown>(logger);
+
+    std::cout << time_report;
 }
